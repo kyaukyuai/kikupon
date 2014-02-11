4cef43bcda024dfa11071b770ea9ef435346d17b
Yuya Kakui <mail@kakuiyuya.com>
2014-02-11 16:27:30 +0900

    install plugin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kikupon::Application.config.secret_key_base = '5a6ce9efcd547a47503363d8222dc1f56c03e9a10814a65995b00b8fb957d4504fc59dc0fdf36112bc1c804f70fa4a04f89ba3c238ae414548d22fae1ba77846'
